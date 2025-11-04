a41f237569e8995a55c1a66805ee1813ab7c28ce
Guilherme Dias <guilherme.dds.dev@gmail.com>
2025-11-04 01:43:22 +0000

    Aula 03-11 (parte 2)

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,7 +2,22 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        NotificacaoSms notifSMS = new NotificacaoSms("Guilherme", "(14) 997568741", "Mensagem teste", 1);
+
+        notifSMS.exibirInformacoesRemetente();
+        notifSMS.enviar();
+        notifSMS.registrarLog();
+
+        notifSMS.definirPrioridade(8);
+        notifSMS.obterNivelPrioridade();
+        notifSMS.definirPrioridade(0);
+
+        Notificacao notif = new NotificacaoSms("Fulano", "14997568741", "Testando", 3);
+        notif.exibirInformacoesRemetente();
+        notif.enviar();
+
+        Priorizavel objPriorizavel = new NotificacaoSms("Ciclano", "14943534593", "Novo teste", 2);
+        objPriorizavel.definirPrioridade(6);
     }
 }
 
